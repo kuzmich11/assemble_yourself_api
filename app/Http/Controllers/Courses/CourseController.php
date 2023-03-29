815113e9b84e9f5881780f08009f4f280f9c26b0
Vladimir Kuznetsov <v.a.kuznetsov-83@yandex.ru>
2023-03-29 19:11:48 +0300

    Подключил swagger

--- app/Http/Controllers/Courses/CourseController.php
+++ app/Http/Controllers/Courses/CourseController.php
@@ -8,6 +8,27 @@ use App\Models\CourseModel;
 use App\Models\User;
 use App\QueryBuilders\CoursesQueryBuilder;
 use Illuminate\Http\Request;
+use OpenApi\Annotations\OpenApi as OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      x={
+ *          "logo": {
+ *              "url": "https://via.placeholder.com/190x90.png?text=L5-Swagger"
+ *          }
+ *      },
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="darius@matulionis.lt"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
 
 class CourseController extends Controller
 {
@@ -30,6 +51,24 @@ class CourseController extends Controller
         return response()->json($course);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/courses",
+     *     tags={"courses"},
+     *     summary="Create course",
+     *     description="This can only be done by the logged in course.",
+     *     operationId="createCourse",
+     *     @OA\Response(
+     *         response="default",
+     *         description="successful operation"
+     *     ),
+     *     @OA\RequestBody(
+     *         description="Create course object",
+     *         required=true,
+     *         @OA\JsonContent(ref="#/Models/CourseModel")
+     *     )
+     * )
+     */
     public function createCourse(CreateRequest $request)
     {
         $user = auth()->user();
