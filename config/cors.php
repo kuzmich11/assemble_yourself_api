9a6e3aafd15bc5dc03df0448a232ece1ec1658da
Vladimir Kuznetsov <v.a.kuznetsov-83@yandex.ru>
2023-04-16 23:02:45 +0300

    ИсÐПоправÑил cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://assemble-yourself-client.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
